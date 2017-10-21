b5da0c047ef0ed935b05323f1438d28641eb13d6
Marcos Paulo Cayres Rosa <mp_cayres96@hotmail.com>
2017-10-21 11:48:11 -0200

    Collider para a lanterna
    Só funciona com quem estiver no Layer Enemies

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -6,11 +6,14 @@ public class Flashlight : MonoBehaviour {
     Player script;
     float rotationSpeed = 1f;
     Light light;
+    Collider2D collider;
 
     void Start ()
     {
         script = GetComponentInParent<Player>();
         light = GetComponent<Light>();
+        collider = GetComponent<Collider2D>();
+        collider.enabled = light.enabled;
     }
 	
 	void Update ()
@@ -19,6 +22,7 @@ public class Flashlight : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.F))
         {
             light.enabled = !light.enabled;
+            collider.enabled = light.enabled;
             if (light.enabled)
             {
                 transform.rotation = Quaternion.Euler((float)0.0, (float)0.0, (float)0.0);
@@ -32,23 +36,27 @@ public class Flashlight : MonoBehaviour {
                 case 0:
                     Quaternion targetRotationE = Quaternion.Euler((float)180.0, (float)230.0, (float)0.0);
                     transform.rotation = Quaternion.Slerp(transform.rotation, targetRotationE, rotationSpeed * Time.deltaTime);
+                    collider.offset = new Vector2(-1, 0);
                     break;
                 case 1:
                     Quaternion targetRotationW = Quaternion.Euler((float)180.0, (float)130.0, (float)0.0);
                     transform.rotation = Quaternion.Slerp(transform.rotation, targetRotationW, rotationSpeed * Time.deltaTime);
+                    collider.offset = new Vector2(1, 0);
                     break;
                 case 2:
                     Quaternion targetRotationN = Quaternion.Euler((float)-45.0, (float)0.0, (float)0.0);
                     transform.rotation = Quaternion.Slerp(transform.rotation, targetRotationN, rotationSpeed * Time.deltaTime);
+                    collider.offset = new Vector2(0, 1);
                     break;
                 case 3:
                     Quaternion targetRotationS = Quaternion.Euler((float)45.0, (float)0.0, (float)0.0);
                     transform.rotation = Quaternion.Slerp(transform.rotation, targetRotationS, rotationSpeed * Time.deltaTime);
+                    collider.offset = new Vector2(0, -1);
                     break;
                 default:
                     break;
             }
         }
-        
-	}
+
+    }
 }
