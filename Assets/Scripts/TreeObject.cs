a122a695c586f33c3d856a6d0368769704bacf1a
Marcos Paulo Cayres Rosa <mp_cayres96@hotmail.com>
2017-10-25 13:22:21 -0200

    Melhora na colisão com árvores

---
+++ Assets/Scripts/TreeObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeObject : MonoBehaviour {
+    SpriteRenderer spriteRenderer;
+    float offset;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        offset = GetComponent<BoxCollider2D>().offset.y 
+            - player.GetComponent<SpriteRenderer>().size.y*player.transform.lossyScale.y/2;
+    }
+
+    void Update()
+    {
+        spriteRenderer.sortingOrder = Mathf.RoundToInt((transform.position.y + offset) * 100f) * -1;
+    }
+}
