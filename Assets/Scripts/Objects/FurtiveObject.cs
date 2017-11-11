f0c809ea5360422a77d881777b2718587a8eb055
Marcos Paulo Cayres Rosa <mp_cayres96@hotmail.com>
2017-11-11 15:55:34 -0200

    Tempo para o objeto furtivo

--- Assets/Scripts/Objects/FurtiveObject.cs
+++ Assets/Scripts/Objects/FurtiveObject.cs
@@ -4,6 +4,7 @@ public class FurtiveObject : MonoBehaviour {
     GameObject player;
     public bool colliding = false;
     SpriteRenderer spriteRenderer;
+    float timeLeft;
 
     void Start ()
     {
@@ -14,23 +15,29 @@ public class FurtiveObject : MonoBehaviour {
     void Update()
     {
         spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+
         if (colliding && !MissionManager.instance.paused)
         {
-            if (Input.GetKeyDown(KeyCode.Z)) //GetKeyDown e GetKeyUp não pode ser usado fora do Update
+            if (timeLeft > 0)
             {
-                if (player.GetComponent<Renderer>().enabled)
-                {
-                    player.GetComponent<Renderer>().enabled = false;
-                    player.layer = LayerMask.NameToLayer("PlayerHidden");
-                    player.GetComponent<MissionManager>().blocked = true;
-                }
-                else
-                {
-                    player.GetComponent<Renderer>().enabled = true;
-                    player.layer = LayerMask.NameToLayer("Player");
-                    player.GetComponent<MissionManager>().blocked = false;
-                }
+                timeLeft -= Time.deltaTime;
+            }
+
+            if (player.GetComponent<Renderer>().enabled && Input.GetKeyDown(KeyCode.Z)) //GetKeyDown e GetKeyUp não pode ser usado fora do Update
+            {
+                player.GetComponent<Renderer>().enabled = false;
+                player.layer = LayerMask.NameToLayer("PlayerHidden");
+                player.GetComponent<MissionManager>().blocked = true;
+                timeLeft = 3;
+            }
+            else if (!player.GetComponent<Renderer>().enabled && (Input.GetKeyDown(KeyCode.Z) || timeLeft <= 0))
+            {
+                player.GetComponent<Renderer>().enabled = true;
+                player.layer = LayerMask.NameToLayer("Player");
+                player.GetComponent<MissionManager>().blocked = false;
+                timeLeft = 0;
             }
         }
+        
     }
 }
