41c5470cbfafbeae480345d70b04113ee6b13d23
Rennê Ruan <140030930@aluno.unb.br>
2017-11-15 14:19:36 -0200

    Início de Lógica de Patrulha da mãe

---
+++ Assets/Scripts/MomPatrolMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomPatrolMovement : MonoBehaviour {
+    public Transform[] targets;
+    private int destPoint = 0;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        GotoNextPoint();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GotoNextPoint();
+	}
+
+    void GotoNextPoint()
+    {
+        float step = speed * Time.deltaTime;
+        
+        if (targets.Length == 0)
+            return;
+
+        transform.position = Vector3.MoveTowards(transform.position, targets[destPoint].position, step);
+
+        float dist = Vector3.Distance(targets[destPoint].position, transform.position);
+        if (dist < 0.4f)
+        {
+            destPoint = (destPoint + 1) % targets.Length;
+        }
+
+    }
+}
