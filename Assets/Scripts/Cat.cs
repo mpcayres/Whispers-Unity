af9084636c1234aa31e47289649fe59e94eede61
Rennê Ruan <140030930@aluno.unb.br>
2017-11-15 18:28:24 -0200

    Gato carente bailando

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -7,6 +7,8 @@ public class Cat : MonoBehaviour {
     public float speed;
     GameObject player;
     public Animator animator;
+    private bool directionChanged = true;
+    private int direction = 0;
 
     // Use this for initialization
     void Start () {
@@ -20,12 +22,52 @@ public class Cat : MonoBehaviour {
         if (followingPlayer)
         {
             float dist = Vector3.Distance(player.transform.position, transform.position);
-            if (dist > 0.6f){
+
+            if (dist > 0.6f)
+            {
                 transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+
+                if (Mathf.Abs(player.transform.position.x - transform.position.x) >
+                    Mathf.Abs(player.transform.position.y - transform.position.y))
+                {
+                    if (player.transform.position.x > transform.position.x)
+                    {
+                        animator.SetInteger("direction", 2);
+                        animator.SetTrigger("changeState");
+                    }
+                    else
+                    {
+                        animator.SetInteger("direction", 3);
+                        animator.SetTrigger("changeState");
+                    }
+                }
+                else { 
+                    if (player.transform.position.y < transform.position.y)
+                    {
+                        animator.SetInteger("direction", 4);
+                        animator.SetTrigger("changeState");
+                    }
+                    else
+                    {
+                        animator.SetInteger("direction", 5);
+                        animator.SetTrigger("changeState");
+                    }
+                }
+
             }
-
-           
-
+            else
+            {
+                if (player.transform.position.y > transform.position.y)
+                {
+                    animator.SetInteger("direction", 1);
+                    animator.SetTrigger("changeState");
+                }
+                else
+                {
+                    animator.SetInteger("direction", 0);
+                    animator.SetTrigger("changeState");
+                }
+            }
         }
 		
 	}
