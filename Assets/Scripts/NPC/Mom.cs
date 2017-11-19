a35d4a401595579c15b4e6316259d94b59d9df3a
Rennê Ruan <140030930@aluno.unb.br>
2017-11-19 17:48:45 -0200

    Alterações mãe (Comentado para começar no quarto da criança)

--- Assets/Scripts/NPC/Mom.cs
+++ Assets/Scripts/NPC/Mom.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MomPatrolMovement : MonoBehaviour {
+public class Mom: MonoBehaviour {
     public Transform[] targets;
     private int destPoint = 0;
     public float speed;
     SpriteRenderer spriteRenderer;
     public Animator animator;
+    public bool isPatroller = false;
     
     void Start () {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -20,7 +21,14 @@ public class MomPatrolMovement : MonoBehaviour {
 	void Update () {
         spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
 
-        GotoNextPoint();
+        if (isPatroller)
+        {
+            GotoNextPoint();
+        }
+        else
+        {
+            animator.SetInteger("direction", 5);
+        }
 	}
 
     void GotoNextPoint()
@@ -45,13 +53,14 @@ public class MomPatrolMovement : MonoBehaviour {
 
     void ChangeDirection()
     {
-        if(Mathf.Abs(targets[destPoint].position.y - transform.position.y) < 0.3){
+        if(Mathf.Abs(targets[destPoint].position.y - transform.position.y) <
+            Mathf.Abs(targets[destPoint].position.x - transform.position.x))
+        {
             if (targets[destPoint].position.x > transform.position.x)
                 animator.SetInteger("direction", 0);
             else
                 animator.SetInteger("direction", 1);
-        }else if(Mathf.Abs(targets[destPoint].position.x - transform.position.x) < 0.3)
-        {
+        }else {
             if (targets[destPoint].position.y > transform.position.y)
                 animator.SetInteger("direction", 2);
             else
