95eaee1953d8a5139a4ac2e3d70711474b923080
Marcos Paulo Cayres Rosa <mp_cayres96@hotmail.com>
2017-12-07 17:01:15 -0200

    Pessoas no corredor na Missão 3

---
+++ Assets/Scripts/NPC/PersonShadow.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonShadow : MonoBehaviour {
+    public Transform[] targets;
+    private int destPoint = 0;
+    public float speed;
+    SpriteRenderer spriteRenderer;
+    public Animator animator;
+    public bool isPatroller = false;
+    public bool destroyEndPath = false;
+    public bool stopEndPath = false;
+    int direction = 0;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+        animator.SetTrigger("changeDirection");
+    }
+
+    void Update()
+    {
+        spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+
+        if (isPatroller)
+        {
+            GotoNextPoint();
+        }
+        else
+        {
+            animator.SetInteger("direction", 5);
+        }
+    }
+
+    void GotoNextPoint()
+    {
+        float step = speed * Time.deltaTime;
+
+        if (targets.Length == 0)
+            return;
+
+        ChangeDirection();
+
+        transform.position = Vector3.MoveTowards(transform.position, targets[destPoint].position, step);
+
+        float dist = Vector3.Distance(targets[destPoint].position, transform.position);
+        if (dist < 0.4f)
+        {
+            animator.SetTrigger("changeDirection");
+            if (destPoint + 1 == targets.Length && destroyEndPath)
+            {
+                Destroy(gameObject);
+            }
+            else if (destPoint + 1 == targets.Length && stopEndPath)
+            {
+                Stop();
+                // Mudar animacao, ficar parado
+            }
+            else
+            {
+                destPoint = (destPoint + 1) % targets.Length;
+            }
+        }
+
+    }
+
+    void ChangeDirection()
+    {
+        if (Mathf.Abs(targets[destPoint].position.y - transform.position.y) <
+            Mathf.Abs(targets[destPoint].position.x - transform.position.x))
+        {
+            if (targets[destPoint].position.x > transform.position.x)
+                direction = 0;
+            else
+                direction = 1;
+        }
+        else
+        {
+            if (targets[destPoint].position.y > transform.position.y)
+                direction = 2;
+            else
+                direction = 3;
+        }
+        animator.SetInteger("direction", direction);
+    }
+
+    public int GetDirection()
+    {
+        return direction;
+    }
+
+    public void Stop()
+    {
+        isPatroller = false;
+    }
+}
