f6748c687f12fd4ad688dd9e39f8bf30925106ee
Rennê Ruan <140030930@aluno.unb.br>
2017-12-08 20:42:34 -0200

    Lógica de passar de página (não testada)

--- Assets/Scripts/Player/Book.cs
+++ Assets/Scripts/Player/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Image = UnityEngine.UI.Image;
 using UnityEngine;
 
 public class Book : MonoBehaviour {
@@ -8,22 +9,85 @@ public class Book : MonoBehaviour {
     private int pageQuantity;
     private int pageShowing; // 0 - 2
   
-    GameObject book;
+    GameObject book, page1, page2;
     MissionManager missionManager;
+
+    Sprite pg1, pg2, pg3, pg4, pg5;
 
 	void Start () {
         pageQuantity = 0;
         book = GameObject.Find("HUDCanvas").transform.Find("Book").gameObject;
+        page1 = GameObject.Find("HUDCanvas").transform.Find("Book/Page 1").gameObject;
+        page2 = GameObject.Find("HUDCanvas").transform.Find("Book/Page 2").gameObject;
         pageShowing = 0;
         missionManager = GameObject.Find("Player").GetComponent<MissionManager>();
+        pg1 = Resources.Load<Sprite>("Sprites/UI/box");
+        pg2 = Resources.Load<Sprite>("Sprites/UI/box");
+        pg3 = Resources.Load<Sprite>("Sprites/UI/box");
+        pg4 = Resources.Load<Sprite>("Sprites/UI/box");
+        pg5 = Resources.Load<Sprite>("Sprites/UI/box");
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.J) && !MissionManager.instance.blocked 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && !MissionManager.instance.blocked
             && !MissionManager.instance.pausedObject)
         {
             ShowBook();
+        }
+
+        if (book.activeSelf && pageQuantity > 0)
+        {
+            if (pageShowing == 0)
+            {
+                if (pages[0] == true)
+                {
+                    page1.SetActive(true);
+                    page1.GetComponent<Image>().sprite = pg1;
+                }
+                if (pages[1] == true)
+                {
+                    page2.SetActive(true);
+                    page2.GetComponent<Image>().sprite = pg2;
+                    //resource da pagina 2
+                }
+            }
+            else if (pageShowing == 1) { 
+                if (pages[2] == true)
+                {
+                    page1.SetActive(true);
+                    page1.GetComponent<Image>().sprite = pg3;
+                }
+                if (pages[3] == true)
+                {
+                    page2.SetActive(true);
+                    page2.GetComponent<Image>().sprite = pg4;
+                }
+            }
+            else if (pageShowing == 2)
+            {
+                if (pages[4] == true)
+                {
+                    page1.SetActive(true);
+                    page1.GetComponent<Image>().sprite = pg5;
+                }
+            }
+        }
+
+        if(book.activeSelf && (Input.GetKeyDown(KeyCode.RightArrow))){
+            if(pageShowing == 0 && pageQuantity > 2 && pageShowing != 2) {
+                page1.SetActive(false); page2.SetActive(false);
+                pageShowing++; 
+            }
+        }
+        if (book.activeSelf && (Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+            if (pageShowing != 0)
+            {
+                page1.SetActive(false); page2.SetActive(false);
+                pageShowing--;
+            }
         }
     }
 
@@ -37,9 +101,11 @@ public class Book : MonoBehaviour {
         else
         {
             book.SetActive(true);
-            missionManager.paused = true;
+            missionManager.paused = true;   
 
         }
     }
+
+
 
 }
