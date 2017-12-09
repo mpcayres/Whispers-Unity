2c8c4bb9d7ff0de6fa04c268f91da710877fb0fd
Rennê Ruan <140030930@aluno.unb.br>
2017-12-09 18:08:13 -0200

    Paginas funcionando e mostrando

--- Assets/Scripts/Player/Book/Book.cs
+++ Assets/Scripts/Player/Book/Book.cs
@@ -5,27 +5,31 @@ using UnityEngine;
 
 public class Book : MonoBehaviour {
 
-    private bool[] pages = { false, false, false, false, false };
-    private int pageQuantity;
+    private static bool[] pages = { false, false, false, false, false };
+    private static int pageQuantity;
     private int pageShowing; // 0 - 2
   
-    GameObject book, page1, page2;
+    GameObject book, page1, page2,pagebonus;
     MissionManager missionManager;
 
-    Sprite pg1, pg2, pg3, pg4, pg5;
+    Sprite pg1, pg2, pg3, pg4, pg5, pg6, pg78;
 
 	void Start () {
         pageQuantity = 0;
         book = GameObject.Find("HUDCanvas").transform.Find("Book").gameObject;
         page1 = GameObject.Find("HUDCanvas").transform.Find("Book/Page 1").gameObject;
         page2 = GameObject.Find("HUDCanvas").transform.Find("Book/Page 2").gameObject;
+        pagebonus = GameObject.Find("HUDCanvas").transform.Find("Book/Page Bonus").gameObject;
         pageShowing = 0;
         missionManager = GameObject.Find("Player").GetComponent<MissionManager>();
-        pg1 = Resources.Load<Sprite>("Sprites/UI/box");
-        pg2 = Resources.Load<Sprite>("Sprites/UI/box");
-        pg3 = Resources.Load<Sprite>("Sprites/UI/box");
-        pg4 = Resources.Load<Sprite>("Sprites/UI/box");
-        pg5 = Resources.Load<Sprite>("Sprites/UI/box");
+        pg1 = Resources.Load<Sprite>("Sprites/UI/book/page1");
+        pg2 = Resources.Load<Sprite>("Sprites/UI/book/page2");
+        pg3 = Resources.Load<Sprite>("Sprites/UI/book/page3");
+        pg4 = Resources.Load<Sprite>("Sprites/UI/book/page4");
+        pg5 = Resources.Load<Sprite>("Sprites/UI/book/page5");
+        pg6 = Resources.Load<Sprite>("Sprites/UI/book/page6");
+        pg78 = Resources.Load<Sprite>("Sprites/UI/book/page7-8");
+
     }
 
     // Update is called once per frame
@@ -71,14 +75,31 @@ public class Book : MonoBehaviour {
                 {
                     page1.SetActive(true);
                     page1.GetComponent<Image>().sprite = pg5;
+                    page2.SetActive(true);
+                    page2.GetComponent<Image>().sprite = pg6;
                 }
+            }
+            else if (pageShowing == 3)
+            {
+                pagebonus.SetActive(true);
+                pagebonus.GetComponent<Image>().sprite = pg78;
             }
         }
 
         if(book.activeSelf && (Input.GetKeyDown(KeyCode.RightArrow))){
-            if(pageShowing == 0 && pageQuantity > 2 && pageShowing != 2) {
+            if(pageShowing == 0 && pageQuantity >= 2 && pageShowing != 2) {
                 page1.SetActive(false); page2.SetActive(false);
                 pageShowing++; 
+            }
+            else if(pageShowing == 1 && pages[4] == true)
+            {
+                page1.SetActive(false); page2.SetActive(false);
+                pageShowing++;
+            }
+            else if(pageShowing == 2 && pageQuantity == 5)
+            {
+                page1.SetActive(false); page2.SetActive(false);
+                pageShowing++;
             }
         }
         if (book.activeSelf && (Input.GetKeyDown(KeyCode.LeftArrow)))
@@ -86,6 +107,7 @@ public class Book : MonoBehaviour {
             if (pageShowing != 0)
             {
                 page1.SetActive(false); page2.SetActive(false);
+                pagebonus.SetActive(false);
                 pageShowing--;
             }
         }
@@ -105,6 +127,13 @@ public class Book : MonoBehaviour {
 
         }
     }
+
+    public static void AddPage(int number)
+    {
+        pages[number] = true;
+        pageQuantity++;
+    }
+
 
 
 
---
+++ Assets/Scripts/Player/Book/Page.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page : MonoBehaviour {
+    public int number;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Book.AddPage(number);
+        Destroy(this.gameObject);
+        
+    }
+}
