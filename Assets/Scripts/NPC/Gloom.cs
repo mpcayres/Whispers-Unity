9d58b97c940eabb2373f6b25dcadd93ee42f9b3b
Rennê Ruan <140030930@aluno.unb.br>
2017-12-10 17:48:16 -0200

    Inicio missão 6 + Sprites mãe doente

---
+++ Assets/Scripts/NPC/Gloom.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gloom : MonoBehaviour
+{
+    private int maxRoar = 5;
+    private void Start()
+    {
+        gameObject.GetComponent<AudioSource>().Stop();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Flashlight.GetState() && !gameObject.GetComponent<AudioSource>().isPlaying)
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+            maxRoar--;
+            if(maxRoar < 0)
+            {
+                MissionManager.instance.GameOver();
+            }
+        }
+    }
+}
