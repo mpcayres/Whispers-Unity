d11c1f43869aecae477301933cde168d8de341a2
ingridsl <inus2agome@gmail.com>
2018-01-28 15:26:22 -0200

    escolha de idioma

---
+++ Assets/ChangeLanguage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPGTALK.Localization;
+using UnityEngine.UI;
+
+public class ChangeLanguage : MonoBehaviour {
+    public int language;
+    private Button button { get { return GetComponent<Button>(); } }
+
+    void Start() {
+
+        button.onClick.AddListener(() => ChangeTextLanguage());
+    }
+
+
+
+    // Update is called once per frame
+    void ChangeTextLanguage()
+
+    {
+        switch (language) {
+            case 1:
+                LanguageSettings.actualLanguage = SupportedLanguages.EN_US;
+                break;
+            case 2:
+                LanguageSettings.actualLanguage = SupportedLanguages.PT_BR;
+                break;
+
+
+        }
+
+    }
+}
--- Assets/Imported/RPGTALK/Scripts/RPGTalkLocalization.cs
+++ Assets/Imported/RPGTALK/Scripts/RPGTalkLocalization.cs
@@ -8,7 +8,7 @@ namespace RPGTALK.Localization
 	/// An Enum with the supported languages. You can edit this enum at will to add more languages to your game
 	/// </summary>
 	public enum SupportedLanguages{
-		EN_US,ES,FR,PT_BR
+		EN_US,ES,FR,PT_BR,JP
 	}
 
 	/// <summary>
