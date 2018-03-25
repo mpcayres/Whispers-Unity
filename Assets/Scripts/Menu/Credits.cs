e3e4e84e3bc07126863754bec85b04e8cd37c7d2
ingridsl <inus2agome@gmail.com>
2018-03-25 13:39:29 -0300

    troca de cena ao terminar créditos

---
+++ Assets/Scripts/Menu/Credits.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+    // Use this for initialization
+    public void ChangeScene (int number = 0) {
+        MissionManager.LoadScene(0); // animation finished: load main menu
+    }
+}
