c47b304e1fbe72a4fc52398e52699df6fdfd7b67
Marcos Paulo Cayres Rosa <mp_cayres96@hotmail.com>
2018-03-31 11:57:04 -0300

    Pequenos ajustes + objeto de ataque

---
+++ Assets/Scripts/Player/InventoryObjects/AttackObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Image = UnityEngine.UI.Image;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class AttackObject : MonoBehaviour {
+
+    public Inventory.InventoryItems item;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+}
