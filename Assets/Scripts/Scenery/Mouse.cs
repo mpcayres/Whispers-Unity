e0d8c31f1d3201646e6fcf5364219aba1eee01d2
ingridsl <inus2agome@gmail.com>
2018-03-31 21:12:58 -0300

    jerry na cozinha

---
+++ Assets/Scripts/Scenery/Mouse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    public Animation animation { get { return GetComponent<Animation>(); } }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            animation.Play("Mouse");
+        }
+    }
+}
