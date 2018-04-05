6a39859b7c4094ea29e289cff4759d029a5c6bbd
Marcos Paulo Cayres Rosa <mp_cayres96@hotmail.com>
2018-04-04 23:15:14 -0300

    Ajustes do tutorial
    Apaga quando sai da região e para quando executa a ação. Não está mais
    dando aqueles tremeliques aqui

--- Assets/Scripts/Scenery/Tutorial.cs
+++ Assets/Scripts/Scenery/Tutorial.cs
@@ -5,84 +5,83 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Tutorial : MonoBehaviour
 {
-    bool again;
     public string keyName1, keyName2;
-    int repeatTime = 2;
-    bool exit = false;
+    public int mission = 1;
     public AudioClip click;
-    bool invoked = false;
     public AudioSource source { get { return GetComponent<AudioSource>(); } }
-    void Start()
+
+    int repeatTime = 2;
+    bool exit = false, end = false, invoked = false;
+
+    void Awake()
     {
-        again = true;
         source.playOnAwake = false;
     }
+
+    void Update()
+    {
+        if (!end && !exit)
+        {
+            KeyPressed();
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (exit)
+        if (other.gameObject.tag.Equals("Player") && MissionManager.instance.currentMission == mission && !end)
         {
-            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        }
-        if (other.gameObject.tag.Equals("Player") && MissionManager.instance.currentMission == 1 && again)
-        {
-            if (!source.isPlaying && !invoked)
-            {
-                Invoke("Show", repeatTime);
-                invoked = true;
-            }
-        }
-        if (CrossPlatformInputManager.GetButtonDown(keyName1) && CrossPlatformInputManager.GetButtonDown(keyName2))
-        {
-            exit = true;
-            again = false;
+            exit = false;
+            InvokeShow();
         }
     }
+
     void OnTriggerStay2D(Collider2D other)
     {
-
-        if (exit)
+        if (other.gameObject.tag.Equals("Player") && MissionManager.instance.currentMission == mission && !end)
         {
-            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        }
-        if (other.gameObject.tag.Equals("Player") && MissionManager.instance.currentMission == 1 && again)
-        {
-            if (!source.isPlaying && !invoked)
-            {
-                Invoke("Show", repeatTime);
-                invoked = true;
-            }
-        }
-        if (CrossPlatformInputManager.GetButtonDown(keyName1) && CrossPlatformInputManager.GetButtonDown(keyName2)){
-            exit = true;
-            again = false;
+            InvokeShow();
         }
     }
+
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.tag.Equals("Player") && MissionManager.instance.currentMission == 1)
+        if (other.gameObject.tag.Equals("Player") && MissionManager.instance.currentMission == mission)
         {
-            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
             exit = true;
+            invoked = false;
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
         }
-        if (CrossPlatformInputManager.GetButtonDown(keyName1) && CrossPlatformInputManager.GetButtonDown(keyName2)){
-            exit = true;
-            again = false;
+    }
+
+    void KeyPressed()
+    {
+        if (CrossPlatformInputManager.GetButtonDown(keyName1) && CrossPlatformInputManager.GetButtonDown(keyName2))
+        {
+            end = true;
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
+    void InvokeShow()
+    {
+        if (!source.isPlaying && !invoked)
+        {
+            Invoke("Show", repeatTime);
             invoked = true;
         }
     }
 
-    public void Show() {
-        if (this.gameObject.transform.GetChild(0).gameObject.activeInHierarchy) {
-            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    void Show() {
+        if (!end && !exit) {
+            if (gameObject.transform.GetChild(0).gameObject.activeInHierarchy) {
+                gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            }
+            else {
+                gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            }
+            source.PlayOneShot(click);
+            invoked = false;
         }
-        else {
-            this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-        }
-        source.PlayOneShot(click);
-        invoked = false;
     }
-
-
-
 
 }
