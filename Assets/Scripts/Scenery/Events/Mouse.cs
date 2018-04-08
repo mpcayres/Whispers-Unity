e65333c79303d72647b94af74433dc64531a3509
ingridsl <inus2agome@gmail.com>
2018-04-08 10:39:20 -0300

    rato reaproveitável e com som

--- Assets/Scripts/Scenery/Events/Mouse.cs
+++ Assets/Scripts/Scenery/Events/Mouse.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Mouse : MonoBehaviour
 {
+    public AudioClip squeak;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    
+    public string animationName = "Mouse";
+    public bool always = true; //after all this time? always
+    bool playedAlready = false;
     public Animation animationMouse { get { return GetComponent<Animation>(); } }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag.Equals("Player"))
+        if (other.gameObject.tag.Equals("Player") && !playedAlready)
         {
-            animationMouse.Play("Mouse");
+            if (!always) {
+                if (MissionManager.instance.currentMission % 2 == 0)
+                {
+                    source.clip = squeak;
+                    source.PlayDelayed(0.5f);
+                    animationMouse.Play(animationName);
+                    playedAlready = true;
+                }
+
+            }
+
+            else{
+                source.clip = squeak;
+                source.PlayDelayed(0.5f);
+                animationMouse.Play(animationName);
+                playedAlready = true;
+
+            }
         }
     }
 }
