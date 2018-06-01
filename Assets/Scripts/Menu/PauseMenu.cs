3ec6a554bfd9180bf0c958eb87b5fc162fec41ce
ingridsl <inus2agome@gmail.com>
2018-05-31 22:15:46 -0300

    Pause menu com acesso ao inventário e controles (por algum motivo, não funciona com setas)

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Image = UnityEngine.UI.Image;
+using Text = UnityEngine.UI.Text;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PauseMenu : MonoBehaviour
+{
+    MissionManager missionManager;
+    public PauseMenuOptions option;
+    public enum PauseMenuOptions {BackToGame, BackToMainMenu };
+    private void Awake()
+    {
+        missionManager = GameObject.Find("Player").GetComponent<MissionManager>();
+    }
+    public void OnClick()
+    {
+        if (option == PauseMenuOptions.BackToGame)
+        {
+
+            missionManager.paused = false;
+            this.transform.parent.gameObject.SetActive(false);
+        }
+        else {
+            MissionManager.LoadScene(0);
+        }
+
+    }
+    
+}
---
+++ Assets/Scripts/Player/Pause.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Image = UnityEngine.UI.Image;
+using Text = UnityEngine.UI.Text;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class Pause: MonoBehaviour
+{
+    public AudioClip sound;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    public GameObject pauseMenu;
+    MissionManager missionManager;
+
+    private void Awake()
+    {
+        pauseMenu = GameObject.Find("HUDCanvas").transform.Find("PauseMenu").gameObject;
+        pauseMenu.SetActive(false);
+        missionManager = GameObject.Find("Player").GetComponent<MissionManager>();
+    }
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
+    }
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("keyPause") && !MissionManager.instance.blocked && !MissionManager.instance.showMissionStart && !MissionManager.instance.pausedObject && !Book.show)
+        {
+            if (!source.isPlaying)
+                source.PlayOneShot(sound);
+
+            if (pauseMenu.activeSelf)
+            {
+                missionManager.paused = false;
+                //missionManager.blocked = false;
+                pauseMenu.SetActive(false);
+            }
+            else
+            {
+                missionManager.paused = true;
+                //missionManager.blocked = true;
+                pauseMenu.SetActive(true);
+            }
+        }
+    }
+
+}
